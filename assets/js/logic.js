a32a943c7d4822fc3b3ceab67790006f46fd20fb
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-01-22 11:15:44 +0000

    add eventListener to Start Quiz button

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+var startQuizButton = document.querySelector("#start");
+
+function startQuiz(event) {
+  // Once this is working, work on getting the questions to display.
+  console.log("`Start Quiz` button has been clicked!");
+}
+
+startQuizButton.addEventListener("click", startQuiz);
