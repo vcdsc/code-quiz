5250d2c2fad1d132744a518ec683c40c2dc07e09
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-01-22 18:13:55 +0000

    add questions as an array of objects

---
+++ assets/js/questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  { title: "1", choices: ["a", "b", "c", "d"], answer: "a" },
+  { title: "2", choices: ["a", "b", "c", "d"], answer: "a" },
+  { title: "3", choices: ["a", "b", "c", "d"], answer: "a" },
+  { title: "4", choices: ["a", "b", "c", "d"], answer: "a" },
+  { title: "5", choices: ["a", "b", "c", "d"], answer: "a" },
+];
