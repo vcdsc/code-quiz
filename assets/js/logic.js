3de7554b5c836bf5835b7e8d8d32b0e2fbc68782
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-01-22 18:15:24 +0000

    add ability to hide and show elements; display questions and append choices; display end screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,53 @@
-var startQuizButton = document.querySelector("#start");
+var divQuestions = document.getElementById("questions");
+var startQuizButton = document.getElementById("start");
+var divStartScreen = document.getElementById("start-screen");
+var questionTitle = document.getElementById("question-title");
+var divChoices = document.getElementById("choices");
+var divEndScreen = document.getElementById("end-screen");
+
+var currentQuestion = 0;
+
+function setElementHidden(element) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  element.classList.add("hide");
+}
+
+function setElementVisible(element) {
+  element.classList.remove("hide");
+}
 
 function startQuiz(event) {
-  // Once this is working, work on getting the questions to display.
-  console.log("`Start Quiz` button has been clicked!");
+  event.preventDefault();
+  setElementHidden(divStartScreen);
+  setElementVisible(divQuestions);
+  renderQuestion(questions[currentQuestion]);
+}
+
+function renderEndScreen() {
+  setElementHidden(divQuestions);
+  setElementVisible(divEndScreen);
+}
+
+function nextQuestion(event) {
+  if (questions.length - 1 !== currentQuestion) {
+    currentQuestion++;
+    renderQuestion(questions[currentQuestion]);
+  } else {
+    renderEndScreen();
+  }
 }
 
 startQuizButton.addEventListener("click", startQuiz);
+divChoices.addEventListener("click", nextQuestion);
+
+function renderQuestion(question) {
+  questionTitle.textContent = question.title;
+  divChoices.innerHTML = "";
+
+  for (var i = 0; i < question.choices.length; i++) {
+    var choice = question.choices[i];
+    var button = document.createElement("button");
+    button.textContent = `${i + 1} ${choice}`;
+    divChoices.appendChild(button);
+  }
+}
