0ea71554f297f46613200eeac4d36b4b015a2002
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-01-23 13:10:15 +0000

    replace placeholder questions with proper questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,42 @@
 var questions = [
-  { title: "1", choices: ["a", "b", "c", "d"], answer: "a" },
-  { title: "2", choices: ["a", "b", "c", "d"], answer: "b" },
-  { title: "3", choices: ["a", "b", "c", "d"], answer: "c" },
-  { title: "4", choices: ["a", "b", "c", "d"], answer: "d" },
-  { title: "5", choices: ["a", "b", "c", "d"], answer: "a" },
+  {
+    title: "Which HTML element do we use to add JavaScript?",
+    choices: ["<script>", "<js>", "<code>", "<papyrus>"],
+    answer: "<script>",
+  },
+  {
+    title: `If you wanted to display "Hello World" in a alert box, how could you do it?`,
+    choices: [
+      `msg("Hello World")`,
+      `alert("Hello World")`,
+      `alertBox("Hello World)`,
+      `textBubble("Hello World)`,
+    ],
+    answer: `alert("Hello World")`,
+  },
+  {
+    title: "Which of the below is a function declaration?",
+    choices: [
+      "function:myFunction()",
+      "function = myFunction()",
+      "function myFunction()",
+      "var = function()",
+    ],
+    answer: "function myFunction()",
+  },
+  {
+    title: "Which of the below is a valid JavaScript array?",
+    choices: [
+      `colors = ["red", "green", "yellow"]`,
+      `var colors = ["red" + "green" + "yellow"]`,
+      `colors = "red, green, blue"`,
+      `var colors = ["red", "green", "yellow"]`,
+    ],
+    answer: `var colors = ["red", "green", "yellow"]`,
+  },
+  {
+    title: "Which event occurs when a user clocks on an HTML element?",
+    choices: ["onClick", "onHover", "onMouseClick", "onClicked"],
+    answer: "onClick",
+  },
 ];
